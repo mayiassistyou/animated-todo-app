3407d51fc5089774fabf6f1312b18be0f21f0cb9
yamngu <phuln8@gmail.com>
2021-11-16 22:06:57 +0700

    animated todo item

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -5,17 +5,25 @@ import TaskItem from '../components/task-item';
 
 export default function MainScreen(){
     const [checked, setChecked] = useState(false);
+    const [ subject, setSubject ] = useState('Task Item')
+    const [ isEditing, setEditing ] = useState(false);
 
     const handlePressCheckbox = useCallback(() => {
         setChecked(prev => !prev)
     }, [])
 
     return(
-        <Center _dark={{bg: 'blueGray.900'}} _light={{bg: 'blueGray.50'}} px={4} flex={1} >
-            <VStack space={5} alignItems="center">
-                <Box w="100px" h="100px">
-                    <TaskItem isDone={checked} onToggleCheckbox={handlePressCheckbox} />
-                </Box>
+        <Center _dark={{bg: 'blueGray.900'}} _light={{bg: 'blueGray.50'}} flex={1} >
+            <VStack space={5} alignItems="center" w="full">
+                <TaskItem
+                    isEditing={isEditing}
+                    isDone={checked} 
+                    onToggleCheckbox={handlePressCheckbox}
+                    subject={subject}
+                    onChangeSubject={setSubject}
+                    onPressLabel={() => setEditing(true)}
+                    onFinishEditing={() => setEditing(false)}
+                />
                 <ThemeToggle />
             </VStack>
         </Center>
